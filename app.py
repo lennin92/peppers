998768438cf43e3221fbabe01ded595fce47a981
lennindga <lennin92@gmail.com>
2016-07-04 15:00:28 -0600

    Agregadas funciones para tratamiento de la RNA

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import logging
 import flask
 
-from utils import dicom_utils
+import caffe_tools as ct
+
+from utils import dicom, image
 
 # Obtain the flask app object
 app = flask.Flask(__name__)
@@ -19,7 +21,7 @@ def classify_upload():
         logging.info('Saving to %s.', filename)
         filename2 = filename+".png"
         dicom_utils.dicom_to_png(filename, filename2)
-        image = exifutil.open_oriented_im(filename2)
+        image = image.open_oriented_im(filename2)
 
     except Exception as err:
         logging.info('Uploaded image open error: %s', err)
@@ -43,3 +45,56 @@ def embed_image_html(image):
     image_pil.save(string_buf, format='png')
     data = string_buf.getvalue().encode('base64').replace('\n', '')
     return 'data:image/png;base64,' + data
+
+
+def allowed_file(filename):
+    return (
+        '.' in filename and
+        filename.rsplit('.', 1)[1] in ALLOWED_IMAGE_EXTENSIONS
+    )
+
+
+def start_tornado(app, port=5000):
+    http_server = tornado.httpserver.HTTPServer(
+        tornado.wsgi.WSGIContainer(app))
+    http_server.listen(port)
+    print("Tornado server starting on port {}".format(port))
+    tornado.ioloop.IOLoop.instance().start()
+
+
+def start_from_terminal(app):
+    """
+    Parse command line options and start the server.
+    """
+    parser = optparse.OptionParser()
+    parser.add_option(
+        '-d', '--debug',
+        help="enable debug mode",
+        action="store_true", default=False)
+    parser.add_option(
+        '-p', '--port',
+        help="which port to serve content on",
+        type='int', default=5000)
+    parser.add_option(
+        '-g', '--gpu',
+        help="use gpu mode",
+        action='store_true', default=False)
+
+    opts, args = parser.parse_args()
+    ImagenetClassifier.default_args.update({'gpu_mode': opts.gpu})
+
+    # Initialize classifier + warm start by forward for allocation
+    app.clf = ImagenetClassifier(**ImagenetClassifier.default_args)
+    app.clf.net.forward()
+
+    if opts.debug:
+        app.run(debug=True, host='0.0.0.0', port=opts.port)
+    else:
+        start_tornado(app, opts.port)
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    start_from_terminal(app)
