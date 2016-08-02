56b965bd4a1ddcc58307cfae8c4e03ec2adb8f5e
lennindga <lennin92@gmail.com>
2016-08-02 15:56:29 -0600

    e

--- utils/dicomutils.py
+++ utils/dicomutils.py
@@ -24,15 +24,17 @@ def extract_png(dicom_path):
             if col > max_val: max_val = col
         image_2d.append(pixels)
     # Rescaling grey scale between 0-255
-    image_2d_scaled = []
-    for row in image_2d:
-        row_scaled = []
-        for col in row:
-            col_scaled = int((float(col) * 255.0/ float(max_val)) )
-            row_scaled.append(col_scaled)
-        image_2d_scaled.append(row_scaled)
+    if max_val>0:
+        image_2d_scaled = []
+        for row in image_2d:
+            row_scaled = []
+            for col in row:
+                col_scaled = int((float(col) * 255.0/ float(max_val)) )
+                row_scaled.append(col_scaled)
+            image_2d_scaled.append(row_scaled)
 
-    return (shape[0], shape[1], image_2d_scaled, )
+        return (shape[0], shape[1], image_2d_scaled, )
+    raise Exception("MAX VAL = 0")
 
 def save_png(pixel_matix, height, width, path):
     png_file = open(path, 'wb')
@@ -43,10 +45,10 @@ def save_png(pixel_matix, height, width, path):
 def dicom_to_png(dicom_path, png_path):
 
     if not os.path.exists(dicom_path):
-        raise Exception("Dicom Path does not exists")
+        raise Exception("Dicom Path does not exists %s"%(dicom_path))
 
     if os.path.exists(png_path):
-        raise Exception("PNG Path already exists")
+        raise Exception("PNG Path already exists %s"%(png_path))
 
     height, width, pixel_matrix = extract_png(dicom_path)
 
--- utils/h5generator.py
+++ utils/h5generator.py
@@ -6,22 +6,21 @@ import os
 
 from settings import SIZE, TMP_PNG_PATH, TRAIN_PATH, TRAINLIST_PATH, CSV_PATH, BASE_PATH
 from dicomutils import dicom_to_png
-import traceback
-
-
+import log
 
 def processPNG(lines, train_path, trainlist_path):
-    X = np.zeros((len(lines), 3, SIZE, SIZE), dtype='f4')
-    y = np.zeros((1, len(lines)), dtype='f4')
+    X = np.zeros((len(lines),  3, SIZE, SIZE), dtype='f4')
+    y = np.zeros((len(lines), 1), dtype='f4')
     for i, l in enumerate(lines):
+        print(l)
         img = caffe.io.load_image(l[0])
-        img = caffe.io.resize(img, (SIZE, SIZE, 3))  # resize to fixed size
+        img = caffe.io.resize(img, (3, SIZE, SIZE))  # resize to fixed size
         # you may apply other input transformations here...
         X[i] = img
         y[i] = int(l[1])
     with h5py.File(train_path, 'w') as H:
-        H.create_dataset('X', data=X)  # note the name X given to the dataset!
-        H.create_dataset('y', data=y)  # note the name y given to the dataset!
+        H.create_dataset('image', data=X)  # note the name X given to the dataset!
+        H.create_dataset('label', data=y)  # note the name y given to the dataset!
     with open(trainlist_path, 'w') as L:
         L.write(train_path)  # list all h5 files you are going to use
 
@@ -30,19 +29,27 @@ def processCSV(csvpath, basepath, train_path, trainlist_path):
     pngs = []
     file_obj = open(csvpath, 'r')
     reader = csv.reader(file_obj)
+    print("CONVERTING DICOM TO PNG")
+    log.info("CONVERTING DICOM TO PNG")
     for row in reader:
-        pngpath = os.path.join(TMP_PNG_PATH, row[0])
+        pngpath = os.path.join(TMP_PNG_PATH, row[0].replace("/",".").replace("\\","."))
         dcmpath = os.path.join(basepath, row[0])
-        if not os.path.exists(pngpath):
-            os.remove(pngpath)
-        print("Storing %s on %s"%(dcmpath, pngpath))
         try:
+            print("GENERATING PNG FOR %s"%(dcmpath))
+            log.info("GENERATING PNG FOR %s"%(dcmpath))
             dicom_to_png(dcmpath, pngpath)
-            pngs.push([pngpath, row[1]])
+            pngs.append([pngpath, row[1]])
+            pngs.append([pngpath, row[1]])
+            print("GENERATED PNG %s"%(pngpath))
+            log.info("GENERATED PNG %s"%(pngpath))
         except Exception, e:
-            print("ERROR ON GENERATE DICOM")
-            print(traceback.format_exc())
+            print("ERROR ON GENERATE DICOM", e)
+            log.error(e)
+    print("GENERATING H5 DATABASE FILE")
+    log.info("GENERATING H5 DATABASE FILE")
     processPNG(pngs, train_path, trainlist_path)
+    print("FINISHED")
+    log.info("FINISHED")
 
 
 if __name__=="__main__":
