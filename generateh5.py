1872056978a152a59ee05afbe7f659e07367c2c9
lennin <lennin92@gmail.com>
2016-08-14 21:59:11 -0600

    Generation of h5 datafile

--- generateh5.py
+++ generateh5.py
@@ -5,16 +5,17 @@ from utils.h5 import create_h5
 from utils.settings import SIZE, TMP_PNG_PATH, \
     CSV_PATH, BASE_PATH, CSV_TESTPNG_PATH, CSV_TRAINPNG_PATH, BATCH_SIZE, H5_PATH
 from utils import log
-
+import traceback
 
 def add_csv(csv_path, lines):
+    lines = [l[0:2] for l in lines]
     with open(csv_path, 'a') as csvf:
         csvw = csv.writer(csvf, delimiter=',')
-        csvw.writerows(lines[0:2])
+        csvw.writerows(lines)
 
 
 def process_dicom_csv(csv_path, base_path, png_base_path, batch_size, fixed_size,
-        h5_path, h5_index_path, png_train_csv_path, png_test_csv_path):
+                      h5_path, h5_index_path, png_train_csv_path, png_test_csv_path):
     # type: (str, str, str, int, int, str, str, str, str) -> None
     pngs_train = []  # 70 % of training samples
     pngs_test = []  # 30 % of training samples
@@ -26,49 +27,67 @@ def process_dicom_csv(csv_path, base_path, png_base_path, batch_size, fixed_size
     count_error = 0
     count_trainh5 = 0
     count_testh5 = 0
-    for row in reader:
-        count += 1
-        dicom_path = os.path.join(base_path, row[0])
-        png_path = os.path.join(png_base_path, row[0].replace("/", ".").replace("\\", "."))
-        try:
-            pixel_matrix = extract_grid(dicom_path)
-            save_png(pixel_matrix, png_path)
+    try:
 
-            if (random.uniform(0.0, 1) < 0.7):
-                count_train += 1
-                pngs_train.append([png_path, row[1], pixel_matrix])
-            else:
-                count_test += 1
-                pngs_test.append([png_path, row[1], pixel_matrix])
+        for row in reader:
+            try:
+                count += 1
+                dicom_path = os.path.join(base_path, row[0])
+                png_path = os.path.join(png_base_path, row[0].replace("/", ".").replace("\\", "."))
+                pixel_matrix = extract_grid(dicom_path)
+                save_png(pixel_matrix, png_path)
 
-            # IF LENGTH OF TRAIN IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
-            if len(pngs_train)>=batch_size:
-                create_h5(pngs_train, h5_path + "train.h5." + str(count_trainh5),
-                    h5_index_path + "trainlist.txt", fixed_size)
-                add_csv(png_train_csv_path, pngs_train)
-                pngs_train = []
-                print("GENERATED TRAIN H5 %s" % (h5_path + "train.h5." + str(count_trainh5),))
-                log.info("GENERATED TRAIN H5 %s" % (h5_path + "train.h5." + str(count_trainh5),))
-                count_trainh5 += 1
+                if (random.uniform(0.0, 1) < 0.7):
+                    count_train += 1
+                    pngs_train.append([png_path, row[1], pixel_matrix])
+                else:
+                    count_test += 1
+                    pngs_test.append([png_path, row[1], pixel_matrix])
 
-            # IF LENGTH OF TEST IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
-            if len(pngs_test) >= batch_size:
-                create_h5(pngs_test, h5_path + "test.h5." + str(count_testh5),
-                    h5_index_path + "testlist.txt", fixed_size)
-                add_csv(png_test_csv_path, pngs_test)
-                pngs_test = []
-                print("GENERATED TEST H5 %s" % (h5_path + "test.h5." + str(count_testh5),))
-                log.info("GENERATED TEST H5 %s" % (h5_path + "test.h5." + str(count_testh5),))
-                count_testh5 += 1
+                # IF LENGTH OF TRAIN IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
+                if len(pngs_train) >= batch_size:
+                    create_h5(pngs_train, h5_path + "/train.h5." + str(count_trainh5),
+                              h5_index_path + "/trainlist.txt", fixed_size)
+                    add_csv(png_train_csv_path, pngs_train)
+                    pngs_train = []
+                    count_trainh5 += 1
 
-            if count % 200 == 0:
-                print("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
-                log.info("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
+                # IF LENGTH OF TEST IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
+                if len(pngs_test) >= batch_size:
+                    create_h5(pngs_test, h5_path + "/test.h5." + str(count_testh5),
+                              h5_index_path + "/testlist.txt", fixed_size)
+                    add_csv(png_test_csv_path, pngs_test)
+                    pngs_test = []
+                    count_testh5 += 1
 
-        except Exception, e:
-            count_error += 1
-            print("ERROR ON PROCESS DICOM '%s'"%(dicom_path,), e)
-            log.error(e)
+                if count % 200 == 0:
+                    print("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
+                    log.info("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
+            except Exception, e:
+                count_error += 1
+                print("ERROR ON PROCESS DICOM '%s'" % (dicom_path,), e)
+                log.error(e)
+                traceback.print_exc()
+
+        # IF LENGTH OF TRAIN IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
+        if len(pngs_train) >= 0:
+            create_h5(pngs_train, h5_path + "/train.h5." + str(count_trainh5), h5_index_path + "/trainlist.txt", fixed_size)
+            add_csv(png_train_csv_path, pngs_train)
+            count_trainh5 += 1
+
+        # IF LENGTH OF TEST IS >= BATCHSIZE, SAVE h5 AND RESTART LIST
+        if len(pngs_test) >= batch_size:
+            create_h5(pngs_test, h5_path + "/test.h5." + str(count_testh5), h5_index_path + "testlist.txt", fixed_size)
+            add_csv(png_test_csv_path, pngs_test)
+            count_testh5 += 1
+
+        print("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
+        log.info("PROCESSED %d, TRAIN: %d, TEST: %d, ERROR: %d" % (count, count_train, count_test, count_error))
+    except Exception, e:
+        count_error += 1
+        print("ERROR ON PROCESS ", e)
+        log.error(e)
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
--- utils/h5.py
+++ utils/h5.py
@@ -1,15 +1,18 @@
 import h5py
 import numpy as np
-
+import log
 
 def create_h5(lines, pathH5, pathIndex, size):
-    X = np.zeros((len(lines), 3, size, size), dtype='f4')
+    X = np.zeros((len(lines), 1, size, size), dtype='f4')
     y = np.zeros((len(lines), 1), dtype='f4')
     for i, l in enumerate(lines):
-        X[i] = l[0]
+        l[2].resize((1,size,size,))
+        X[i] = l[2]
         y[i] = l[1]
-    with h5py.File(pathH5, 'wb') as H:
+    with h5py.File(pathH5, 'w') as H:
         H.create_dataset('data', data=X)
         H.create_dataset('label', data=y)
     with open(pathIndex, 'a') as index:
         index.write(pathH5+"\n")
+    print("GENERATED H5 %s "%(pathH5))
+    log.info("GENERATED H5 %s "%(pathH5))
