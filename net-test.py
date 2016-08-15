dac94ace59fe044a16c81053c80599e9bdd5a102
lennindga <lennin92@gmail.com>
2016-08-15 09:46:07 -0600

    actualizacion de modelos

--- net-test.py
+++ net-test.py
@@ -4,7 +4,12 @@ import sys
 sys.path.append(CAFFE_PATH)
 
 import caffe
-import cv2
+
+try:
+    import cv2
+except ImportError as E:
+    import cv as cv2
+
 import numpy as np
 import os
 # Set the right path to your model definition file, pretrained model weights,
@@ -12,7 +17,9 @@ import os
 IMAGE_FILE = 'PT0.ST0.SE1.IM9_180'
 
 caffe.set_mode_cpu()
-net = caffe.Net('model/convnet.prototxt', caffe.TEST) #DPLOY_MODEL_FILE, caffe.TEST)
+net = caffe.Net(DPLOY_MODEL_FILE, caffe.TEST)
+                # 'model/convnet.prototxt', caffe.TEST)
+                # DPLOY_MODEL_FILE, caffe.TEST)
                 # '/home/lennin92/dicom/caffe/caffe_sgd_iter_24.caffemodel',
                 # caffe.TEST)
 
@@ -25,11 +32,15 @@ for bl in net.blobs:
     print "    BLOB '%s' SHAPE "%(bl), [s for s in net.blobs[bl].shape]
 
 # plt.imshow(input_image)
-img = cv2.imread('prbdata/' + IMAGE_FILE + '.png', cv2.IMREAD_COLOR)
+img = cv2.imread('prbdata/' + IMAGE_FILE + '.png', cv2.IMREAD_GRAYSCALE)
+print 'img shape = ',img.shape
+cv2.imwrite("prbdata/plots/input_%s.png"%(IMAGE_FILE), 255*img)
 img.resize(BASE_SHAPE)
+print 'img after reshape = ',img.shape
 # img_blobinp = img[np.newaxis, np.newaxis, :, :]
 # net.blobs['image'].reshape(*img_blobinp.shape)
 net.blobs['data'].data[...] = img
+cv2.imwrite("prbdata/plots/data_%s.png"%(IMAGE_FILE), 255*net.blobs['data'].data[0,0])
 
 for bl in net.blobs:
     net.forward()
